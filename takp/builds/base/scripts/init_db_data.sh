eb2920d94fe994d3d1b1002446a84211850dc0bd
HiTechHippie <hitechhippie.gaming@gmail.com>
2023-10-27 19:24:07 -0700

    new db drop

--- takp/builds/base/scripts/init_db_data.sh
+++ takp/builds/base/scripts/init_db_data.sh
@@ -12,13 +12,13 @@ else
   echo "[database] grant privs on ${DB_NAME_LS} database..."
   mariadb --user root --password=${MARIADB_ROOT_PASSWORD} -h db -e "GRANT ALL PRIVILEGES ON ${DB_NAME_LS}.* TO '${MARIADB_USER}'@'%'"
   echo "[database] load alkabor tables..."
-  mariadb --user root --password=${MARIADB_ROOT_PASSWORD} -h db -D ${DB_NAME_GAME} < /data/alkabor_2023-10-11-15\:19.sql
+  mariadb --user root --password=${MARIADB_ROOT_PASSWORD} -h db -D ${DB_NAME_GAME} < /data/alkabor_2023-10-25-19\:31.sql
   echo "[database] load player tables..."
-  mariadb --user root --password=${MARIADB_ROOT_PASSWORD} -h db -D ${DB_NAME_GAME} < /data/player_tables_2023-10-11-15\:19.sql
+  mariadb --user root --password=${MARIADB_ROOT_PASSWORD} -h db -D ${DB_NAME_GAME} < /data/player_tables_2023-10-25-19\:31.sql
   echo "[database] load data tables..."
-  mariadb --user root --password=${MARIADB_ROOT_PASSWORD} -h db -D ${DB_NAME_GAME} < /data/data_tables_2023-10-11-15\:19.sql
+  mariadb --user root --password=${MARIADB_ROOT_PASSWORD} -h db -D ${DB_NAME_GAME} < /data/data_tables_2023-10-25-19\:31.sql
   echo "[database] load loginserver tables..."
-  mariadb --user root --password=${MARIADB_ROOT_PASSWORD} -h db -D ${DB_NAME_LS} < /data/login_tables_2023-10-11-15\:19.sql
+  mariadb --user root --password=${MARIADB_ROOT_PASSWORD} -h db -D ${DB_NAME_LS} < /data/login_tables_2023-10-25-19\:31.sql
   mariadb --user root --password=${MARIADB_ROOT_PASSWORD} -h db -D ${DB_NAME_LS} < /data/tblloginserversettings.sql
   mariadb --user root --password=${MARIADB_ROOT_PASSWORD} -h db -D ${DB_NAME_LS} -e "ALTER TABLE tblLoginServerAccounts ALTER creationIP SET DEFAULT '127.0.0.1';"
   mariadb --user root --password=${MARIADB_ROOT_PASSWORD} -h db -D ${DB_NAME_LS} -e "ALTER TABLE tblLoginServerAccounts ALTER ForumName SET DEFAULT 'docker';"
